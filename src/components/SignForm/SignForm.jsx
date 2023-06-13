980ba698f8166fe2500145101e3c240ba33b9968
hrafnrir <anjarodion@gmail.com>
2023-06-13 21:38:53 +0300

    Add dispatching sign up action

--- src/components/SignForm/SignForm.jsx
+++ src/components/SignForm/SignForm.jsx
@@ -1,7 +1,9 @@
+import { useDispatch } from "react-redux";
 import { Form, Formik } from "formik";
 import PropTypes from "prop-types";
 
 import { signUpValidation, signInValidation } from "./formValidation.js";
+import sagaActions from "../../model/sagas/actions.js";
 
 import { TextInput, PasswordInput } from "./FormElements.jsx";
 
@@ -21,12 +23,20 @@ const initialValues = {
 };
 
 const SignForm = ({ isSignUp }) => {
+  const dispatch = useDispatch();
   const submitBtnValue = isSignUp ? "Create account" : "Login";
+
+  const actionType = isSignUp ? sagaActions.SIGNUP : sagaActions.SIGNIN;
+
+  const handleSubmit = (values) => {
+    dispatch({ type: actionType, payload: { ...values } });
+  };
 
   return (
     <Formik
       initialValues={isSignUp ? initialValues.SIGNUP : initialValues.SIGNIN}
       validationSchema={isSignUp ? signUpValidation : signInValidation}
+      onSubmit={(values) => handleSubmit(values)}
     >
       <Form className={s.form}>
         <TextInput type="text" name="username" placeholder="Username*" />
