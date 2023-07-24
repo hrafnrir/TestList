6ef3b706be9763fc92b3e4412780df85be6225f9
hrafnrir <anjarodion@gmail.com>
2023-07-24 18:54:10 +0300

    Add props to QuestionElement component

--- src/components/QuestionElement/QuestionElement.jsx
+++ src/components/QuestionElement/QuestionElement.jsx
@@ -1,21 +1,30 @@
+import PropTypes from "prop-types";
 import cn from "classnames";
 
 import s from "./styles/QuestionElement.module.scss";
 
-const QuestionElement = () => {
+const QuestionElement = ({ title, onFormOpen, onRemove }) => {
   return (
     <div className={s.question}>
-      <h3 className={s.questionHeading}>Choose one of the suggested answers</h3>
+      <h3 className={s.questionHeading}>{title}</h3>
       <button
         className={cn(s.questionBtn, s.questionBtn_edit)}
         type="button"
+        onClick={onFormOpen}
       ></button>
       <button
         className={cn(s.questionBtn, s.questionBtn_delete)}
         type="button"
+        onClick={onRemove}
       ></button>
     </div>
   );
 };
 
 export default QuestionElement;
+
+QuestionElement.propTypes = {
+  title: PropTypes.string.isRequired,
+  onFormOpen: PropTypes.func.isRequired,
+  onRemove: PropTypes.func.isRequired,
+};
